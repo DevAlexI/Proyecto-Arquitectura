6bc92d9d99f9672ffebabc6376ffdc994f21cfd1
devlex <48767282+DevAlexI@users.noreply.github.com>
2021-12-05 17:57:23 -0600

    changing test p tag

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.send('<p>HTML Data</p>');
+  res.send('<p>HTML Datamami</p>');
 });
 
 module.exports = router;
