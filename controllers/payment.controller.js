d73a08874c76d949ed67f7101d04af08a235178d
devlex <48767282+DevAlexI@users.noreply.github.com>
2021-12-14 16:24:28 -0600

    Adding fixes and new features into controllers files

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -1,27 +1,45 @@
-const PAYMENT_FILE_PATH = 'payment-generated';
+const path = require('path');
+const PAYMENT_FILE_PATH = path.resolve('./payment-generated.txt');
 const faker = require('faker');
 const fs = require('fs');
 const LINE_ENDING = require('os').EOL;
+const { reject } = require('lodash');
 
 module.exports = {
     create: function (req, res) {
+        let price = faker.commerce.price();
         const fd = fs.openSync(PAYMENT_FILE_PATH, 'a');
-        fs.appendFileSync(fd, faker.commerce.price() + LINE_ENDING, 'utf8');
-        res.status(201).send();
+        fs.appendFileSync(fd, price + LINE_ENDING, 'utf8');
+        return res.status(201).send(price);
     },
 
     applyDiscount: function (req, res) {
+        let counter = 0;
+        let discounts = [];
+        let discount, total;
         //debera de restar una cantidad a cada precio en payment-generated.txt
-        res.json({ message: ""});
+        fs.readFile(PAYMENT_FILE_PATH, {encoding: 'utf8'}, (err, data) => {
+            if(err) throw err;
+            var payments = data.toString().split("\n");
+            for(i in payments) {
+                if(payments[i] != ''){
+                    counter++;
+                    discount = faker.commerce.price();
+                    total = parseFloat(payments[i]) - parseFloat(discount);
+                    discounts.push({ "ID": counter, "Original": payments[i], "Discount": discount, "Total": total});    
+                }
+            }
+        
+            return res.status(200).send({discounts});
+        });
     },
 
     getPromos: function (req, res) {
-        req.json([
-            {name: "BUENFIN"},
-            {name: "HOTSALE"},
-            {name: "CYBERMONDAY"},
-            {name: "BLACKFRIDAY"},
-            {name: "PRIMEDAY"},
-        ]);
+        res.json([{id: 1, name: 'BUENFIN'},
+                  {id: 2, name: 'HOTSALE'}, 
+                  {id: 3, name: 'CYBERMONDAY'}, 
+                  {id: 4, name: 'BLACKFRIDAY'}, 
+                  {id: 5, name: 'PRIMEDAY'}, 
+                ]);
     }
 };
--- controllers/shipment.controller.js
+++ controllers/shipment.controller.js
@@ -1,13 +1,40 @@
 //deberas de utilizar faker para generar los datos
+const path = require('path');
+const faker = require('faker');
+const fs = require('fs');
+const LINE_ENDING = require('os').EOL;
+const SHIPMENT_FILE_PATH = path.resolve('./shipment-generated.txt');
+
 module.exports = {
     createShipment: function (req, res) {
+        let shipments = [];
         //debera de simular un envio con dirección un precio y una persona con sus datos
-        res.status(201).send();
+        const fd = fs.openSync(SHIPMENT_FILE_PATH, 'a');
+        shipments.push({
+            "Direccion": faker.address.streetAddress(),
+            "Precio": faker.commerce.price(),
+            "Persona":
+            {
+                "Nombre completo": faker.name.firstName() + ' ' + faker.name.lastName(),
+                "Correo": faker.internet.email(),
+                "Tarjeta credito": faker.finance.creditCardNumber()
+            }
+        });
+
+        fs.appendFileSync(fd, JSON.stringify(shipments) + LINE_ENDING, 'utf8');
+        return res.status(201).send({shipments});
     },
+
     changeStatus: function (req, res) {
+        let status =[];
         //Debera de retornar una dirección random
         // codigo de respuesta 201
         // data la direcciòn random
-        res.json({addres: ""});
-    },
+        //res.json({addres: ""});
+        let data = faker.address.streetAddress();
+        status.push({
+            "Address": data
+        });
+        res.status(201).json({status});
+    }
 };
