2a70f807eac34e8827ea7c41153a3a3d17c4cef0
devlex <48767282+DevAlexI@users.noreply.github.com>
2021-12-14 13:29:40 -0600

    Adding missing routes in app and routes files

--- routes/operations.js
+++ routes/operations.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var router = express.Router();
-var tasksController = require('../controllers/tasks.controller');
-var middleware = require('../middleware');
+var opsController = require('../controllers/operations.controller');
+
+//var tasksController = require('../controllers/tasks.controller');
+//var middleware = require('../middleware');
 
 
-router.get('/:id', tasksController.getById);
+//router.get('/:id', tasksController.getById);
 
-router.use(middleware);
+//router.use(middleware);
 
 router
-    .post('/', tasksController.create)
-    .get('/', tasksController.getAll);
+    .get('/sum', opsController.sum);
+
+router
+    .get('/substract', opsController.substract);
+
+router
+    .get('/multiply', opsController.multiply);
+
+router
+    .get('/divide', opsController.divide);
 
 module.exports = router;
--- routes/payment.js
+++ routes/payment.js
@@ -10,3 +10,18 @@ router
     .get('/promos', paymentController.getPromos);
 
 module.exports = router;
+var express = require('express');
+var router = express.Router();
+var paymentController = require('../controllers/payment.controller');
+var middleware = require('../middleware');
+
+
+//router.use(middleware);
+
+router
+    .get('/discounts', paymentController.applyDiscount);
+
+router
+    .get('/promos', paymentController.getPromos);
+
+module.exports = router;
