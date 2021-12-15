758a72f329c5d0ef94e2cf8deea42fe7ac8769f9
devlex <48767282+DevAlexI@users.noreply.github.com>
2021-12-14 18:58:27 -0600

    Quick fixes in routes folder for tests passing

--- routes/payment.js
+++ routes/payment.js
@@ -7,18 +7,6 @@ var middleware = require('../middleware');
 //router.use(middleware);
 
 router
-    .get('/promos', paymentController.getPromos);
-
-module.exports = router;
-var express = require('express');
-var router = express.Router();
-var paymentController = require('../controllers/payment.controller');
-var middleware = require('../middleware');
-
-
-//router.use(middleware);
-
-router
     .get('/discounts', paymentController.applyDiscount);
 
 router
